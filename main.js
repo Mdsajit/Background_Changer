9e4e2aabfe4292bee74d3977ce5d014b5296e7bd
Sajid Wahid <84800802+Mdsajit@users.noreply.github.com>
2022-07-22 21:35:16 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// project requerment:
+// Change the color background
+
+// step 1= create onload function
+window.onload = () =>{
+    main();
+}
+
+function main(){
+    const root = document.getElementById('root')
+    const btn = document.getElementById('change-btn')
+
+    btn.addEventListener('click', function(){
+        const bgColor = generateRGBColor();
+        root.style.background = bgColor;
+    })
+}
+
+// step 2= random color generator function
+function generateRGBColor(){
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
+
+    return  `rgb(${red}, ${green}, ${blue})`
+}
+// step 3= collect all necessary reference
+
+// step 4 = handle the click event
